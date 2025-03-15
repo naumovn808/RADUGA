d86d3ec38a22bb64007a61322f46887aea8f695f
nick <aaa>
2025-03-15 15:27:10 +0500

    Updates

---
+++ js/test.js
@@ -0,0 +1,22 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["test"],{
+
+/***/ "./helpers/test.js":
+/*!*************************!*\
+  !*** ./helpers/test.js ***!
+  \*************************/
+/***/ (function(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   test: function() { return /* binding */ test; }
+/* harmony export */ });
+function test() {
+  console.log("test");
+}
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=test.js.map
