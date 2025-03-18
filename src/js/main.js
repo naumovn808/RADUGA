8c7752f9e78f4bc9381b67c283c6b0eec1bad332
Malikov-Afruz <afruzmalikov65@gmail.com>
2025-03-19 01:54:49 +0500

    make map section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 	init();
 	setSize();
+	map();
 });
 
 function setSize() {
@@ -12,9 +13,8 @@ function setSize() {
 	document.documentElement.style.setProperty(`--vh`, `${vh}px`);
 }
 
-
 async function init() {
-	if (document.querySelector('.nav-lang-dropdown')) {
+	if (document.querySelector(".nav-lang-dropdown")) {
 		window.addEventListener(`click`, (e) => {
 			if (document.querySelector(`details[open]`)) {
 				let target = e.target.closest(`details[open]`);
@@ -28,28 +28,52 @@ async function init() {
 		});
 	}
 
-	if (document.querySelector('.nav__btn')) {
-		const menuBtn = document.querySelector('.nav__btn');
-		const navMenu = document.querySelector('.nav__bottom');
+	if (document.querySelector(".nav__btn")) {
+		const menuBtn = document.querySelector(".nav__btn");
+		const navMenu = document.querySelector(".nav__bottom");
 
-		const body = document.querySelector('body');
+		const body = document.querySelector("body");
 
 		if (menuBtn && navMenu) {
-			menuBtn.addEventListener('click', (e) => {
-
+			menuBtn.addEventListener("click", (e) => {
 				e.preventDefault();
-				body.classList.toggle('open');
-				menuBtn.classList.toggle('open');
-				if (navMenu) navMenu.classList.toggle('open');
+				body.classList.toggle("open");
+				menuBtn.classList.toggle("open");
+				if (navMenu) navMenu.classList.toggle("open");
 			});
 
-			window.addEventListener('resize', () => {
+			window.addEventListener("resize", () => {
 				if (window.innerWidth > 999) {
-					menuBtn.classList.remove('open');
-					body.classList.remove('open');
-					if (navMenu) navMenu.classList.remove('open');
+					menuBtn.classList.remove("open");
+					body.classList.remove("open");
+					if (navMenu) navMenu.classList.remove("open");
 				}
 			});
 		}
 	}
-};
+}
+function map() {
+	const pins = document.querySelectorAll(".pin");
+	pins.forEach((pin) => {
+		pin.addEventListener("mouseover", () => {
+			const pinId = pin.getAttribute("data-pin");
+			const desc = document.querySelector(`.desc.${pinId}`);
+			const slice = document.querySelectorAll(`.map-slice.${pinId}`);
+			console.log(slice);
+			if (desc) {
+				desc.classList.add("active");
+				slice.forEach((item) => item.classList.add("active"));
+			}
+		});
+
+		pin.addEventListener("mouseout", () => {
+			const pinId = pin.getAttribute("data-pin");
+			const desc = document.querySelector(`.desc.${pinId}`);
+			const slice = document.querySelectorAll(`.map-slice.${pinId}`);
+			if (desc) {
+				desc.classList.remove("active");
+				slice.forEach((item) => item.classList.remove("active"));
+			}
+		});
+	});
+}
